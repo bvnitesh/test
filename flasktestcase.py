a59e241ebfcdad7a3b650bb6271049bf65b5e700
bvnitesh <nitesh.b.v.11@gmail.com>
2018-07-18 22:03:34 +0530

    added flasktest

---
+++ flasktestcase.py
@@ -0,0 +1,20 @@
+import requests
+import unittest
+import flasktest
+
+#class TestFlask(unittest.TestCase):
+#      def test_flask(self):
+#          self.assertEqual(requests.get("http://127.0.0.1:5000").text , "Hello World!")
+
+class TestIntegrations(unittest.TestCase):
+    def setUp(self):
+        self.app = flasktest.app.test_client()
+
+    def test_thing(self):
+        response = self.app.get('/')
+        self.assertEqual(response.get_data(), b'Hello World!')
+
+if __name__ == "__main__":
+     unittest.main()
+
+
